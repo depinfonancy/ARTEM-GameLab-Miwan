d885acef8545bc47097296ea9cd364d3d81a2728
leo-hardy <40430728+leo-hardy@users.noreply.github.com>
2019-04-30 19:07:56 +0200

    Moves updated

---
+++ Assets/Scripts/PlayerController1.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Contrôleur du joueur
+/// </summary>
+public class PlayerScript : MonoBehaviour
+{
+  /// <summary>
+  /// 1 - La vitesse de déplacement
+  /// </summary>
+  public Vector2 speed = new Vector2(50, 50);
+    // store Component of the Player GameObject that need to be used in the script
+    private SpriteRenderer m_SpriteRenderer;
+    private Rigidbody2D m_Rigidbody;
+    private Animator m_Animator;
+    private CapsuleCollider2D m_Capsule;
+
+    // 2 - Stockage du mouvement
+    private Vector2 movement;
+
+    private void Awake()
+    {
+        // get the component that will be used at each Update/FixedUpdate
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+        /*NEW*/
+        m_Capsule = GetComponent<CapsuleCollider2D>();
+    }
+
+        void Update()
+  {
+    // 3 - Récupérer les informations du clavier/manette
+    float inputX = Input.GetAxis("Horizontal");
+    float inputY = Input.GetAxis("Vertical");
+
+    // 4 - Calcul du mouvement
+    movement = new Vector2(
+      speed.x * inputX,
+      speed.y * inputY);
+
+  }
+
+  void FixedUpdate()
+  {
+    // 5 - Déplacement
+    m_Rigidbody.velocity = movement;
+  }
+}
