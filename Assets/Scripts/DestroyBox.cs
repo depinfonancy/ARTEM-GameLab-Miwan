8062292c02cebc083a1c424d716edefead09d16f
camille-sivelle <40430753+camille-sivelle@users.noreply.github.com>
2019-04-30 13:50:57 +0200

    added two scenes

---
+++ Assets/Scripts/DestroyBox.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyBox : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("" + col.attachedRigidbody.velocity);
+        if (col.gameObject.name == "caisseBois")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public GameObject toObject;
+    public Vector3 origPoint;
+    float distance;
+    bool reached = false;
+
+    public void Start()
+    {
+        origPoint = transform.position;
+    }
+
+    public void Update()
+    {
+        if (!reached)
+        {
+            distance = Vector3.Distance(transform.position, toObject.transform.position);
+            if (distance > .1)
+            {
+                move(transform.position, toObject.transform.position);
+            }
+            else
+            {
+                reached = true;
+            }
+        }
+        else
+        {
+            distance = Vector3.Distance(transform.position, origPoint);
+            if (distance > .1)
+            {
+                move(transform.position, origPoint);
+            }
+            else
+            {
+                reached = false;
+            }
+        }
+    }
+
+    void move(Vector3 pos, Vector3 towards)
+    {
+        transform.position = Vector3.MoveTowards(pos, towards, .1f);
+    }
+}
