388177866c191a0fc1e6af69a8d893bf1b8958f0
nonodepinfo <noemie.laguelle@gmail.com>
2019-05-12 22:46:20 +0200

    Décors et animation basiques

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Contrôleur du joueur
+/// </summary>
+public class PlayerScript : MonoBehaviour
+{
+    /// <summary>
+    /// 1 - La vitesse de déplacement
+    /// </summary>
+    public Vector2 speed = new Vector2(50, 50);
+
+    // 2 - Stockage du mouvement
+    private Vector2 movement;
+
+    void Update()
+    {
+        // 3 - Récupérer les informations du clavier/manette
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        // 4 - Calcul du mouvement
+        movement = new Vector2(
+          speed.x * inputX,
+          speed.y * inputY);
+
+    }
+
+    void FixedUpdate()
+    {
+        // 5 - Déplacement
+        GetComponent<Rigidbody2D>().velocity = movement;
+    }
+}
