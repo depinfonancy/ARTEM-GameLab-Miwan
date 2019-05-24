dd4bda802673f93a1f90468a19f16b9880d3b81c
kouihara66 <kouihara66@gmail.com>
2019-05-24 16:09:05 +0200

    upgrade

---
+++ Assets/Scripts/ButtonDépart.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class ButtonDépart : MonoBehaviour
+
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+   public void PushStartButton()
+    {
+//        Destroy(GameObject.Find("Singleton"));
+        
+        SceneManager.LoadScene("TitleScene");
+     
+
+
+    }
+
+
+
+
+}
+
+
+
---
+++ Assets/Scripts/ButtonPlay.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ButtonPlay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PushStartButton()
+    {
+        SceneManager.LoadScene("TitleScene");    //playのシーンへ
+    }
+}
