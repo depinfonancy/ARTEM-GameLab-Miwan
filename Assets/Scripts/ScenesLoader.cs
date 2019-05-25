9a171a2f0699ca2a4609302a63cc9ac18951e69d
camille-sivelle <40430753+camille-sivelle@users.noreply.github.com>
2019-05-25 21:09:42 +0200

    Update ScenesLoader.cs

--- Assets/Scripts/ScenesLoader.cs
+++ Assets/Scripts/ScenesLoader.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -11,7 +11,14 @@ public class ScenesLoader : MonoBehaviour
     private string scenenb;
     void Start()
     {
-        
+        transform.position = transform.position - new Vector3(5, 0, 0);
+        //permet d'aligner  l'objet avec la fin du décor, en considérant les paramètres suivants :
+        // - joueur en Z = 0
+        // - background à Z = 5
+        // - caméra en Z = -10
+        // - angle de vue à 60.0f
+        // - écran en 16/9
+        // Si une de ses valeurs change, refaire les math. (là ça tombe preque tout pil à 5 mais c'est du bol)   
     }
 
     // Update is called once per frame
