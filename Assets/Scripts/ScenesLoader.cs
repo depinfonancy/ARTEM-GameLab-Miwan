e2dd93d304118e9d10c05b389850198aed2bb42a
camille-sivelle <40430753+camille-sivelle@users.noreply.github.com>
2019-05-25 21:07:54 +0200

    Add files via upload

---
+++ Assets/Scripts/ScenesLoader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ScenesLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private string scenenb;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //SceneManager.LoadScene(scenenb);
+
+    }
+    //When arriving at the end of the scene
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log(col.gameObject.name);
+        if (col.gameObject.name == "SceneEnd") {
+            SceneManager.LoadScene(scenenb);
+        }
+    }
+
+
+}
