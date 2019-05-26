9f62129e799ca7442e6e04159885eff0c4dddf0f
Anas <anastheone@users.noreply.github.com>
2019-05-26 07:17:21 +0200

    Menu Fini

---
+++ Assets/Scripts/MenuButton.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButton : MonoBehaviour
+{
+    [SerializeField] MenuButtonController menuButtonController;
+    public Animator animator;
+    [SerializeField] AnimatorFunctions animatorFunctions;
+    [SerializeField] int thisIndex;
+    public Rigidbody2D miwan;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (menuButtonController.index == thisIndex)
+        {
+            animator.SetBool("selected", true);
+            if (Input.GetAxis("Submit") == 1)
+            {
+                animator.SetBool("pressed", true);
+            }
+            else if (animator.GetBool("pressed"))
+            {
+                animator.SetBool("pressed", false);
+                animatorFunctions.disableOnce = true;
+
+                if (thisIndex == 0)
+                {
+                    menuButtonController.canvas.enabled = false;
+                    miwan = GameObject.FindWithTag("Player").GetComponent<Rigidbody2D>();
+                    miwan.isKinematic = false;
+                }
+                else if (thisIndex == 2) Application.Quit();
+            }
+
+
+        }
+        else
+        {
+            animator.SetBool("selected", false);
+        }
+    }
+
+}
---
+++ Assets/dontDestroy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dontDestroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("musique");
+        if (objs.Length > 1) Destroy(this.gameObject);
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+}
